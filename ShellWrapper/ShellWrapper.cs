f6bec93d9af5a96f34db8673bd218b63e8bfe26a
ᦖєgιт <support@legitsoulja.info>
2018-05-22 15:42:12 -0400

    Add files via upload

---
+++ ShellWrapper/ShellWrapper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace ShellWrapper
+{
+
+    public static class ShellWrapper
+    {
+
+        public static void Debug(string[] args)
+        {
+
+            SendMessage(0x052C); // activate workerw process on shell
+            IntPtr workerw = GetShellWorker();
+            Console.WriteLine(workerw.ToString());
+            Console.WriteLine("Drawing will begin after a key press");
+            Console.ReadKey();
+            drawTest(workerw);
+            Console.WriteLine("Did it work? :). Press enter to exit now.");
+            Console.ReadLine();
+
+            clearGraphics(workerw);
+
+            Console.ReadKey();
+            Environment.Exit(0);
+
+        }
+
+        private static void drawTest(IntPtr worker)
+        {
+            IntPtr dc = W32.GetDCEx(worker, IntPtr.Zero, (W32.DeviceContextValues)0x403);
+
+            if (dc != IntPtr.Zero)
+            {
+                using(Graphics g = Graphics.FromHdc(dc))
+                {
+                    g.FillRectangle(new SolidBrush(Color.Red), 0, 0, 500, 500);
+                }
+
+                W32.ReleaseDC(worker, dc);
+
+            }
+        }
+
+
+        public static void clearGraphics(IntPtr worker)
+        {
+
+            // W32.RedrawWindow(worker, IntPtr.Zero, IntPtr.Zero, W32.RedrawWindowFlags.Invalidate);
+            // W32.InvalidateRect(worker, IntPtr.Zero, true);
+
+            var wpReg = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", false);
+            var wallpaperPath = wpReg.GetValue("WallPaper").ToString();
+            wpReg.Close();
+
+            if (System.IO.File.Exists(wallpaperPath))
+            {
+                W32.SystemParametersInfo(W32.SPI_SETDESKWALLPAPER, 0, wallpaperPath, W32.SPIF_UPDATEINIFILE);
+            }
+
+        }
+
+        public static IntPtr GetShellWorker()
+        {
+            IntPtr worker = IntPtr.Zero;
+            W32.EnumWindows(new W32.EnumWindowsProc((th, tph) => {
+
+                IntPtr p = W32.FindWindowEx(th, IntPtr.Zero, "SHELLDLL_DefView", null);
+
+                if(p != IntPtr.Zero)
+                {
+                    worker = W32.FindWindowEx(IntPtr.Zero, th, "WorkerW", null);
+                }
+
+                return true;
+
+            }), IntPtr.Zero);
+
+            return worker;
+        }
+
+        public static void SendMessage(uint address)
+        {
+            UIntPtr result;
+            W32.SendMessageTimeout(W32.FindWindow("Progman", null), address, new UIntPtr(0), IntPtr.Zero, W32.SendMessageTimeoutFlags.SMTO_NORMAL, 1000, out result);
+            Console.WriteLine(result.ToString());
+        }
+
+    }
+
+}
---
+++ ShellWrapper/W32.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellWrapper
+{
+    public static class W32
+    {
+
+        /// <summary>
+        ///     Retrieves a handle to the top-level window whose class name and window name match the specified strings. This
+        ///     function does not search child windows. This function does not perform a case-sensitive search. To search child
+        ///     windows, beginning with a specified child window, use the
+        ///     <see cref="!:https://msdn.microsoft.com/en-us/library/windows/desktop/ms633500%28v=vs.85%29.aspx">FindWindowEx</see>
+        ///     function.
+        ///     <para>
+        ///     Go to https://msdn.microsoft.com/en-us/library/windows/desktop/ms633499%28v=vs.85%29.aspx for FindWindow
+        ///     information or https://msdn.microsoft.com/en-us/library/windows/desktop/ms633500%28v=vs.85%29.aspx for
+        ///     FindWindowEx
+        ///     </para>
+        /// </summary>
+        /// <param name="lpClassName">
+        ///     C++ ( lpClassName [in, optional]. Type: LPCTSTR )<br />The class name or a class atom created by a previous call to
+        ///     the RegisterClass or RegisterClassEx function. The atom must be in the low-order word of lpClassName; the
+        ///     high-order word must be zero.
+        ///     <para>
+        ///     If lpClassName points to a string, it specifies the window class name. The class name can be any name
+        ///     registered with RegisterClass or RegisterClassEx, or any of the predefined control-class names.
+        ///     </para>
+        ///     <para>If lpClassName is NULL, it finds any window whose title matches the lpWindowName parameter.</para>
+        /// </param>
+        /// <param name="lpWindowName">
+        ///     C++ ( lpWindowName [in, optional]. Type: LPCTSTR )<br />The window name (the window's
+        ///     title). If this parameter is NULL, all window names match.
+        /// </param>
+        /// <returns>
+        ///     C++ ( Type: HWND )<br />If the function succeeds, the return value is a handle to the window that has the
+        ///     specified class name and window name. If the function fails, the return value is NULL.
+        ///     <para>To get extended error information, call GetLastError.</para>
+        /// </returns>
+        /// <remarks>
+        ///     If the lpWindowName parameter is not NULL, FindWindow calls the <see cref="M:GetWindowText" /> function to
+        ///     retrieve the window name for comparison. For a description of a potential problem that can arise, see the Remarks
+        ///     for <see cref="M:GetWindowText" />.
+        /// </remarks>
+        // For Windows Mobile, replace user32.dll with coredll.dll
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        // Find window by Caption only. Note you must pass IntPtr.Zero as the first parameter.
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        public static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
+
+        // You can also call FindWindow(default(string), lpWindowName) or FindWindow((string)null, lpWindowName)
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        // Delegate to filter which windows to include 
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+
+
+        /// <summary> Get the text for the window pointed to by hWnd </summary>
+        public static string GetWindowText(IntPtr hWnd)
+        {
+            int size = GetWindowTextLength(hWnd);
+            if (size > 0)
+            {
+                var builder = new StringBuilder(size + 1);
+                GetWindowText(hWnd, builder, builder.Capacity);
+                return builder.ToString();
+            }
+
+            return String.Empty;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDCEx(IntPtr hWnd, IntPtr hrgnClip, DeviceContextValues flags);
+
+
+        [Flags]
+        public enum SendMessageTimeoutFlags : uint
+        {
+            SMTO_NORMAL = 0x0,
+            SMTO_BLOCK = 0x1,
+            SMTO_ABORTIFHUNG = 0x2,
+            SMTO_NOTIMEOUTIFNOTHUNG = 0x8,
+            SMTO_ERRORONEXIT = 0x20
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [Flags()]
+        public enum DeviceContextValues : uint
+        {
+            /// <summary>DCX_WINDOW: Returns a DC that corresponds to the window rectangle rather 
+            /// than the client rectangle.</summary>
+            Window = 0x00000001,
+            /// <summary>DCX_CACHE: Returns a DC from the cache, rather than the OWNDC or CLASSDC 
+            /// window. Essentially overrides CS_OWNDC and CS_CLASSDC.</summary>
+            Cache = 0x00000002,
+            /// <summary>DCX_NORESETATTRS: Does not reset the attributes of this DC to the 
+            /// default attributes when this DC is released.</summary>
+            NoResetAttrs = 0x00000004,
+            /// <summary>DCX_CLIPCHILDREN: Excludes the visible regions of all child windows 
+            /// below the window identified by hWnd.</summary>
+            ClipChildren = 0x00000008,
+            /// <summary>DCX_CLIPSIBLINGS: Excludes the visible regions of all sibling windows 
+            /// above the window identified by hWnd.</summary>
+            ClipSiblings = 0x00000010,
+            /// <summary>DCX_PARENTCLIP: Uses the visible region of the parent window. The 
+            /// parent's WS_CLIPCHILDREN and CS_PARENTDC style bits are ignored. The origin is 
+            /// set to the upper-left corner of the window identified by hWnd.</summary>
+            ParentClip = 0x00000020,
+            /// <summary>DCX_EXCLUDERGN: The clipping region identified by hrgnClip is excluded 
+            /// from the visible region of the returned DC.</summary>
+            ExcludeRgn = 0x00000040,
+            /// <summary>DCX_INTERSECTRGN: The clipping region identified by hrgnClip is 
+            /// intersected with the visible region of the returned DC.</summary>
+            IntersectRgn = 0x00000080,
+            /// <summary>DCX_EXCLUDEUPDATE: Unknown...Undocumented</summary>
+            ExcludeUpdate = 0x00000100,
+            /// <summary>DCX_INTERSECTUPDATE: Unknown...Undocumented</summary>
+            IntersectUpdate = 0x00000200,
+            /// <summary>DCX_LOCKWINDOWUPDATE: Allows drawing even if there is a LockWindowUpdate 
+            /// call in effect that would otherwise exclude this window. Used for drawing during 
+            /// tracking.</summary>
+            LockWindowUpdate = 0x00000400,
+            /// <summary>DCX_USESTYLE: Undocumented, something related to WM_NCPAINT message.</summary>
+            UseStyle = 0x00010000,
+            /// <summary>DCX_VALIDATE When specified with DCX_INTERSECTUPDATE, causes the DC to 
+            /// be completely validated. Using this function with both DCX_INTERSECTUPDATE and 
+            /// DCX_VALIDATE is identical to using the BeginPaint function.</summary>
+            Validate = 0x00200000,
+        }
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessageTimeout(
+            IntPtr hWnd,
+            uint Msg,
+            UIntPtr wParam,
+            IntPtr lParam,
+            SendMessageTimeoutFlags fuFlags,
+            uint uTimeout,
+            out UIntPtr lpdwResult);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessageTimeout(
+            IntPtr windowHandle,
+            uint Msg,
+            IntPtr wParam,
+            IntPtr lParam,
+            SendMessageTimeoutFlags flags,
+            uint timeout,
+            out IntPtr result);
+
+        /* Version specifically setup for use with WM_GETTEXT message */
+
+        [DllImport("user32.dll", EntryPoint = "SendMessageTimeout", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern uint SendMessageTimeoutText(
+            IntPtr hWnd,
+            int Msg,              // Use WM_GETTEXT
+            int countOfChars,
+            StringBuilder text,
+            SendMessageTimeoutFlags flags,
+            uint uTImeoutj,
+            out IntPtr result);
+
+        /// <summary> Find all windows that match the given filter </summary>
+        /// <param name="filter"> A delegate that returns true for windows
+        ///    that should be returned and false for windows that should
+        ///    not be returned </param>
+        public static IEnumerable<IntPtr> FindWindows(EnumWindowsProc filter)
+        {
+            IntPtr found = IntPtr.Zero;
+            List<IntPtr> windows = new List<IntPtr>();
+
+            EnumWindows(delegate (IntPtr wnd, IntPtr param)
+            {
+                if (filter(wnd, param))
+                {
+                    // only add the windows that pass the filter
+                    windows.Add(wnd);
+                }
+
+                // but return true here so that we iterate all windows
+                return true;
+            }, IntPtr.Zero);
+
+            return windows;
+        }
+
+        /// <summary> Find all windows that contain the given title text </summary>
+        /// <param name="titleText"> The text that the window title must contain. </param>
+        public static IEnumerable<IntPtr> FindWindowsWithText(string titleText)
+        {
+            return FindWindows(delegate (IntPtr wnd, IntPtr param)
+            {
+                return GetWindowText(wnd).Contains(titleText);
+            });
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern Int32 SystemParametersInfo(UInt32 uiAction, UInt32 uiParam, String pvParam, UInt32 fWinIni);
+        public static UInt32 SPI_SETDESKWALLPAPER = 20;
+        public static UInt32 SPIF_UPDATEINIFILE = 0x1;
+
+        [Flags()]
+        public enum RedrawWindowFlags : uint
+        {
+            /// <summary>
+            /// Invalidates the rectangle or region that you specify in lprcUpdate or hrgnUpdate.
+            /// You can set only one of these parameters to a non-NULL value. If both are NULL, RDW_INVALIDATE invalidates the entire window.
+            /// </summary>
+            Invalidate = 0x0001,
+
+            /// <summary>Causes the OS to post a WM_PAINT message to the window regardless of whether a portion of the window is invalid.</summary>
+            InternalPaint = 0x0002,
+
+            /// <summary>
+            /// Causes the window to receive a WM_ERASEBKGND message when the window is repainted.
+            /// Specify this value in combination with the RDW_INVALIDATE value; otherwise, RDW_ERASE has no effect.
+            /// </summary>
+            Erase = 0x0004,
+
+            /// <summary>
+            /// Validates the rectangle or region that you specify in lprcUpdate or hrgnUpdate.
+            /// You can set only one of these parameters to a non-NULL value. If both are NULL, RDW_VALIDATE validates the entire window.
+            /// This value does not affect internal WM_PAINT messages.
+            /// </summary>
+            Validate = 0x0008,
+
+            NoInternalPaint = 0x0010,
+
+            /// <summary>Suppresses any pending WM_ERASEBKGND messages.</summary>
+            NoErase = 0x0020,
+
+            /// <summary>Excludes child windows, if any, from the repainting operation.</summary>
+            NoChildren = 0x0040,
+
+            /// <summary>Includes child windows, if any, in the repainting operation.</summary>
+            AllChildren = 0x0080,
+
+            /// <summary>Causes the affected windows, which you specify by setting the RDW_ALLCHILDREN and RDW_NOCHILDREN values, to receive WM_ERASEBKGND and WM_PAINT messages before the RedrawWindow returns, if necessary.</summary>
+            UpdateNow = 0x0100,
+
+            /// <summary>
+            /// Causes the affected windows, which you specify by setting the RDW_ALLCHILDREN and RDW_NOCHILDREN values, to receive WM_ERASEBKGND messages before RedrawWindow returns, if necessary.
+            /// The affected windows receive WM_PAINT messages at the ordinary time.
+            /// </summary>
+            EraseNow = 0x0200,
+
+            Frame = 0x0400,
+
+            NoFrame = 0x0800
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left, Top, Right, Bottom;
+
+            public RECT(int left, int top, int right, int bottom)
+            {
+                Left = left;
+                Top = top;
+                Right = right;
+                Bottom = bottom;
+            }
+
+            public RECT(System.Drawing.Rectangle r) : this(r.Left, r.Top, r.Right, r.Bottom) { }
+
+            public int X
+            {
+                get { return Left; }
+                set { Right -= (Left - value); Left = value; }
+            }
+
+            public int Y
+            {
+                get { return Top; }
+                set { Bottom -= (Top - value); Top = value; }
+            }
+
+            public int Height
+            {
+                get { return Bottom - Top; }
+                set { Bottom = value + Top; }
+            }
+
+            public int Width
+            {
+                get { return Right - Left; }
+                set { Right = value + Left; }
+            }
+
+            public System.Drawing.Point Location
+            {
+                get { return new System.Drawing.Point(Left, Top); }
+                set { X = value.X; Y = value.Y; }
+            }
+
+            public System.Drawing.Size Size
+            {
+                get { return new System.Drawing.Size(Width, Height); }
+                set { Width = value.Width; Height = value.Height; }
+            }
+
+            public static implicit operator System.Drawing.Rectangle(RECT r)
+            {
+                return new System.Drawing.Rectangle(r.Left, r.Top, r.Width, r.Height);
+            }
+
+            public static implicit operator RECT(System.Drawing.Rectangle r)
+            {
+                return new RECT(r);
+            }
+
+            public static bool operator ==(RECT r1, RECT r2)
+            {
+                return r1.Equals(r2);
+            }
+
+            public static bool operator !=(RECT r1, RECT r2)
+            {
+                return !r1.Equals(r2);
+            }
+
+            public bool Equals(RECT r)
+            {
+                return r.Left == Left && r.Top == Top && r.Right == Right && r.Bottom == Bottom;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj is RECT)
+                    return Equals((RECT)obj);
+                else if (obj is System.Drawing.Rectangle)
+                    return Equals(new RECT((System.Drawing.Rectangle)obj));
+                return false;
+            }
+
+            public override int GetHashCode()
+            {
+                return ((System.Drawing.Rectangle)this).GetHashCode();
+            }
+
+            public override string ToString()
+            {
+                return string.Format(System.Globalization.CultureInfo.CurrentCulture, "{{Left={0},Top={1},Right={2},Bottom={3}}}", Left, Top, Right, Bottom);
+            }
+        }
+
+
+
+        [DllImport("user32.dll")]
+        public static extern bool RedrawWindow(IntPtr hWnd, [In] ref RECT lprcUpdate, IntPtr hrgnUpdate, RedrawWindowFlags flags);
+
+        [DllImport("user32.dll")]
+        public static extern bool RedrawWindow(IntPtr hWnd, IntPtr lprcUpdate, IntPtr hrgnUpdate, RedrawWindowFlags flags);
+
+        [DllImport("user32.dll")]
+        public static extern bool InvalidateRect(IntPtr hWnd, IntPtr lpRect, bool bErase);
+
+    }
+}
